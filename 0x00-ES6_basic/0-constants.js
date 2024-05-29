b63eb619f0b1d32e1deef5af0207ba9a9729de6e
Opoku-Wiafe <opokuwiafevan@gmail.com>
2024-05-29 09:42:24 +0000

    first task

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-// modify from var to const
 export function taskFirst() {
         const task = 'I prefer const when I can.';
         return task;
 }
 
-// getLast function is already correct
 export function getLast() {
         return ' is okay';
 }
 
-//
 export function taskNext() {
         let combination = 'But sometimes let';
         combination += getLast();
